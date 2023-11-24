ce4ccc74be4a2b38064ff1cd788933e4c56a2c71
Hashini Liyanage <33922245+hashinil@users.noreply.github.com>
2023-11-24 16:25:38 +0100

    Update Other_Number_Line_Jumps.py

--- Other_Number_Line_Jumps.py
+++ Other_Number_Line_Jumps.py
@@ -24,3 +24,22 @@ def kangaroo(x1, v1, x2, v2):
 
 print(kangaroo(0, 2, 0, 2))
 print(kangaroo(0, 2, 5, 3))
+
+
+def kangaroo(x1, v1, x2, v2):
+    # Check if kangaroos have the same starting position and same jump distance
+    if x1 == x2 and v1 == v2:
+        return "YES"
+    # Check if kangaroos have the same starting position but different jump distances
+    elif x1 == x2 and v1 != v2:
+        return "NO"
+    # Check if kangaroos will never meet based on their relative positions and jump distances
+    elif (x1 - x2) % (v2 - v1) == 0 and (x1 - x2) / (v2 - v1) >= 0:
+        return "YES"
+    else:
+        return "NO"
+
+# Example usage:
+x1, v1, x2, v2 = map(int, input().split())
+result = kangaroo(x1, v1, x2, v2)
+print(result)
