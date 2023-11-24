d628457ad6597f45dd3197089d9ebd39e126228c
Hashini Liyanage <33922245+hashinil@users.noreply.github.com>
2023-11-24 12:31:03 +0100

    Update Dictionaries_Hashmaps_checkMagazine.py

--- Dictionaries_Hashmaps_checkMagazine.py
+++ Dictionaries_Hashmaps_checkMagazine.py
@@ -13,3 +13,35 @@ def checkMagazine(magazine, note):
 magazine = ["give", "me", "one", "grand", "today", "night"]
 note = ["Give", "one", "grand", "today"]
 checkMagazine(magazine, note)
+
+
+
+answer2
+
+
+from collections import Counter
+#
+# Complete the 'checkMagazine' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY magazine
+#  2. STRING_ARRAY note
+#
+
+def checkMagazine(magazine, note):
+    # Create Counter objects for both magazine and note
+    magazine_counter = Counter(magazine)
+    note_counter = Counter(note)
+
+    # Check if the magazine has enough words to form the ransom note
+    for word, count in note_counter.items():
+        if magazine_counter[word] < count:
+            print("No")
+            return
+
+    print("Yes")
+
+# Example usage:
+magazine = "attack at dawn".split()
+note = "Attack at dawn".split()
+checkMagazine(magazine, note)
